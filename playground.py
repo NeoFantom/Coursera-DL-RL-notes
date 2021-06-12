319cf5164b039113b6c920d998a78228b271218d
Neo <neohuiqixue@gmail.com>
2021-06-12 11:09:56 +0800

    selenium webdriver

--- playground.py
+++ playground.py
@@ -1,4 +1,11 @@
-import numpy as np
-import matplotlib.pyplot as plt
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-plt.plot(1, )
+options = webdriver.ChromeOptions()
+options.add_argument("user-data-dir=C:\\Users\\18364\\AppData\\Local\\Google\\Chrome\\User Data\\Default")
+driver = webdriver.Chrome(chrome_options=options)
+driver.get("https://www.coursera.org/learn/neural-networks-deep-learning/programming/isoAV/python-basics-with-numpy/lab")
+
+# driver = webdriver.Chrome()
+# driver.get('https://www.coursera.org/learn/neural-networks-deep-learning/programming/isoAV/python-basics-with-numpy/lab')
+
